82934cf1bbd2827f7df8af4c30179b729e2d6e57
aviadkim <aviad@kimfo-fs.com>
2025-01-05 14:53:38 +0200

    Update App.tsx with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-// src/App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PDFTest from './components/PDFTest';
+import ThanksPage from './components/ThanksPage';
 
 function App() {
   return (
-    <div className="App">
-      <PDFTest />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<PDFTest />} />
+          <Route path="/thanks" element={<ThanksPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
