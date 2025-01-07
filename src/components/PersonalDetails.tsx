f883b4831dd359efa85ee93b0ef3c909ad91e620
aviadkim <aviad@kimfo-fs.com>
2025-01-07 12:08:09 +0200

    Fix PersonalDetails component TypeScript implementation

---
+++ src/components/PersonalDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PersonalDetailsProps } from '../types/props';
+
+const PersonalDetails: React.FC<PersonalDetailsProps> = ({ data, onDataChange }) => {
+  // Rest of your existing component code here
+  return (
+    // Your existing JSX
+  );
+};
+
+export default PersonalDetails;
